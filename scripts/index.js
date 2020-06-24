9b5f71cfb8b48d2e04cfe45ab6283c8cbd843639
Lucas <lucas.r2737@Gmail.com>
2020-06-24 01:35:31 -0300

    adds onclick in 'Home' and 'Projetos' buttons

--- scripts/index.js
+++ scripts/index.js
@@ -7,13 +7,37 @@ var speed = 125 /* The speed/duration of the effect in milliseconds */
 function typeWriter() {
     if (!typeWriterCalled){
         if (i < txt.length) {
-            console.log(i)
             document.getElementById("text").innerHTML += txt.charAt(i)
             i++
             setTimeout(typeWriter, speed)
         } else {
             typeWriterCalled = true
             document.getElementById("headerNavBar").style.display = "block"
+            buttonsClick()
+        }
+    }
+}
+
+function welcomeTitle() {
+    document.getElementById("text").innerHTML = 'Olá, seja bem vindx!'
+    document.getElementById("headerNavBar").style.display = "block"
+    buttonsClick()
+}
+
+function buttonsClick() {
+    console.log("OI")
+    var buttons = document.getElementsByClassName("headerLi")
+    for (i in buttons) {
+        if (i == 0) {
+            buttons[i].onclick = function() {
+                window.location="index.html"
+            }
+        } else if (i == 1) {
+            buttons[i].onclick = function() {
+                window.location="projects.html"
+            }
+        } else if (i == 2) {
+            
         }
     }
 }
