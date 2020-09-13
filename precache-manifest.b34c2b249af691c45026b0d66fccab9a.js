4060a5bd562aec9a17d49a26694fbacdf95b2d37
lucasrsv <lucas.r2737@gmail.com>
2020-09-13 16:58:52 -0300

    Updates

---
+++ precache-manifest.b34c2b249af691c45026b0d66fccab9a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac0f433852850bcb22011ca8300e7bd1",
+    "url": "/my-website/index.html"
+  },
+  {
+    "revision": "e2e61c2361f81d101d59",
+    "url": "/my-website/static/css/main.355af2fc.chunk.css"
+  },
+  {
+    "revision": "591eaf787f0770727353",
+    "url": "/my-website/static/js/2.8d47c99a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-website/static/js/2.8d47c99a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2e61c2361f81d101d59",
+    "url": "/my-website/static/js/main.1b11f4fc.chunk.js"
+  },
+  {
+    "revision": "952fb02cf73b2d439434",
+    "url": "/my-website/static/js/runtime-main.1b02d065.js"
+  },
+  {
+    "revision": "c5a49c020477c0675f66464b5291e30c",
+    "url": "/my-website/static/media/alemdacura.c5a49c02.png"
+  },
+  {
+    "revision": "f084aa657f276d8219d03bd147644418",
+    "url": "/my-website/static/media/bonte.f084aa65.png"
+  },
+  {
+    "revision": "b17570df8840553bf1e5fc621b9f3995",
+    "url": "/my-website/static/media/cecine.b17570df.png"
+  },
+  {
+    "revision": "8504a8e9636594872be2b7e4ddc9676a",
+    "url": "/my-website/static/media/comfortaa_bold.8504a8e9.ttf"
+  },
+  {
+    "revision": "37830005fa2aade9bccc8e66e9a440a4",
+    "url": "/my-website/static/media/coursera_angular.37830005.jpg"
+  },
+  {
+    "revision": "738b65d11aa3add64d00008b8a9f037a",
+    "url": "/my-website/static/media/coursera_bootstrap4.738b65d1.jpg"
+  },
+  {
+    "revision": "cc1f7680af3a17a4fba8196a80b80256",
+    "url": "/my-website/static/media/coursera_web.cc1f7680.jpg"
+  },
+  {
+    "revision": "5665014ba84779b13b0c2312a72e32ce",
+    "url": "/my-website/static/media/me.5665014b.png"
+  },
+  {
+    "revision": "573c849ad95e3324c1f49ef765f1688d",
+    "url": "/my-website/static/media/nti.573c849a.jpg"
+  },
+  {
+    "revision": "582cf677690dbd1dddba72a3863a4719",
+    "url": "/my-website/static/media/uptime.582cf677.jpg"
+  }
+]);
